2b85fcf413e339893dfb9670846354780e80eaf7
ChrisM <chrismay4115@gmail.com>
2025-09-16 15:45:47 -0400

    fixed formatting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -36,38 +36,41 @@ export default function Home() {
             Projects & Socials:
           </h1>
         </div>
-        <a
-          href="https://trading-simulator-3gbtpvahktdgkgxwqojvdv.streamlit.app/"
-          target="_blank"
-          className="group mx-auto mb-8 block max-w-xs space-y-3 rounded-lg bg-white p-6 shadow-lg ring-1 ring-slate-900/5 hover:bg-sky-500 hover:ring-sky-500"
-        >
-          <div className="flex items-center space-x-3">
-            <h3 className="text-sm font-semibold text-slate-900 group-hover:text-white">
-              Onnatank - Python, Streamlit, Pandas
-            </h3>
-          </div>
-          <p className="text-sm text-slate-500 group-hover:text-white">
-            A Python-based backtesting simulator that runs and compares two
-            classic algorithmic trading strategies: momentum and mean reversion,
-            using real-world historical stock data.
-          </p>
-        </a>
-        <a
-          href="https://www.onnatank.com/"
-          target="_blank"
-          className="group mx-auto mb-8 block max-w-xs space-y-3 rounded-lg bg-white p-6 shadow-lg ring-1 ring-slate-900/5 hover:bg-sky-500 hover:ring-sky-500"
-        >
-          <div className="flex items-center space-x-3">
-            <h3 className="text-sm font-semibold text-slate-900 group-hover:text-white">
-              Onnatank - TS, React, MySQL
-            </h3>
-          </div>
-          <p className="text-sm text-slate-500 group-hover:text-white">
-            Onnatank is a simple web based tool that allows a user to enter
-            their vehicle&apos;s information as well as a location to see how
-            far they can travel &quot;On(n)-a-tank&quot; of gas.
-          </p>
-        </a>
+        <div className="flex flex-col items-center gap-6 md:flex-row md:justify-center">
+          <a
+            href="https://trading-simulator-3gbtpvahktdgkgxwqojvdv.streamlit.app/"
+            target="_blank"
+            className="group mb-8 w-full max-w-xs space-y-3 rounded-lg bg-white p-6 shadow-lg ring-1 ring-slate-900/5 hover:bg-sky-500 hover:ring-sky-500"
+          >
+            <div className="flex items-center space-x-3">
+              <h3 className="text-sm font-semibold text-slate-900 group-hover:text-white">
+                Backtesting Trading Simulator - Python, Streamlit, Pandas
+              </h3>
+            </div>
+            <p className="text-sm text-slate-500 group-hover:text-white">
+              A Python-based backtesting simulator that runs and compares two
+              classic algorithmic trading strategies: momentum and mean
+              reversion, using real-world historical stock data.
+            </p>
+          </a>
+
+          <a
+            href="https://www.onnatank.com/"
+            target="_blank"
+            className="group mb-8 w-full max-w-xs space-y-3 rounded-lg bg-white p-6 shadow-lg ring-1 ring-slate-900/5 hover:bg-sky-500 hover:ring-sky-500"
+          >
+            <div className="flex items-center space-x-3">
+              <h3 className="text-sm font-semibold text-slate-900 group-hover:text-white">
+                Onnatank - TS, React, MySQL
+              </h3>
+            </div>
+            <p className="text-sm text-slate-500 group-hover:text-white">
+              Onnatank is a simple web based tool that allows a user to enter
+              their vehicle&apos;s information as well as a location to see how
+              far they can travel &quot;On(n)-a-tank&quot; of gas.
+            </p>
+          </a>
+        </div>
         <div className="flex space-x-10">
           <div>
             <a
